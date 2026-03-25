7b1cf47880eebd98b5d285812d85e60e83648a1b
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-03-24 18:37:09 -0600

    created the login page, but still needs all the functionalities.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,4 @@
-import SiteLogo from "@/app/ui/site-logo";
-import LoginForm from '@/app/ui/login-form';
+import CreateAccountForm from '@/app/ui/create-account-form';
 import { Suspense } from 'react';
 import { Metadata } from 'next';
 
@@ -13,11 +12,11 @@ export default function LoginPage() {
       <div className='relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:mt-32'>
         <div className='flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36'>
           <div className='w-32 text-white md:w-36'>
-            <SiteLogo />
+            {/* <SiteLogo /> */}
           </div>
         </div>
           <Suspense>
-            <LoginForm />
+            <CreateAccountForm />
           </Suspense>
       </div>
     </main>
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Merriweather } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-header' });
+
+export const merriweather = Merriweather({ weight: ['400', '700'], subsets: ['latin'], display: 'swap', variable: '--font-paragraph'});
