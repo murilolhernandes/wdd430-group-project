9c58081a99ce7b956595ccf67a8bb1bd38280599
Toyin Ajiboye <toy505050@yahoo.com>
2026-04-08 19:24:58 +0100

    Delete .pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440 directory
    deleyted

--- .pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440/.pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440/.pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440/src/app/ui/site-logo.tsx
+++
@@ -1,9 +0,0 @@
-import { inter } from '@/app/ui/fonts';
-
-export default function SiteLogo() {
-  return (
-    <div className={`${inter.className} flex flex-row items-center leading-none text-white`}>
-      <p className='text-2xl font-bold tracking-wide text-stone-800 md:text-3xl'>Handcrafted Haven</p>
-    </div>
-  );
-}
--- .pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440/.pnpm-store/v10/projects/a4ddb959321dbbacd8f4fff84d453440/src/app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter, Merriweather } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-header' });
-
-export const merriweather = Merriweather({ weight: ['400', '700'], subsets: ['latin'], display: 'swap', variable: '--font-paragraph'});
